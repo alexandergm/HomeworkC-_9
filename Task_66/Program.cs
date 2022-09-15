ccfc5343a199e678a0793e1b6a35f426193cf4d9
Alexander_Vasilyev <sasha_261@outlook.com>
2022-09-16 01:10:05 +0300

    Complete Task 66

---
+++ Task_66/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SummaRec(b, a));
+
+int SummaRec(int N, int M)
+{
+    if (N == M) return M;
+    return N + SummaRec(N - 1, M);
+}
+
