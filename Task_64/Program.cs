08728178c72fdacd99fcabc0e20afa5263789e92
Alexander_Vasilyev <sasha_261@outlook.com>
2022-09-16 01:04:15 +0300

    Complete Task 64

---
+++ Task_64/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Sum(b, a));
+
+string Sum(int N, int M)
+{
+    if (N == M) return $"{M} ";
+    return Sum(N - 1, M) + N + " ";
+}
