ebb87df4b17a758ec39a961726a4c61177c25368
Alexander_Vasilyev <sasha_261@outlook.com>
2022-09-16 01:24:32 +0300

    Complete Task 68

---
+++ Task_68/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число M: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write(AkkermanFunc(n, m)); 
+
+int AkkermanFunc(int n, int m)
+{
+    if (n < 0 || m < 0) throw new ArgumentOutOfRangeException("Вы ввели отрицательное число");
+    if (n == 0) return m + 1;
+    else if ((n != 0) && (m == 0)) return AkkermanFunc(n - 1, 1);
+    else return AkkermanFunc(n - 1, AkkermanFunc(n, m - 1));
+}
